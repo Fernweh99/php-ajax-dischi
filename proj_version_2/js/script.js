139c9b4a7bfba9d5587be4f0a399ccbbefb5cd0e
Fernweh99 <daniele.dimento@gmail.com>
2022-09-01 16:28:51 +0200

    fix and finish bonus

--- proj_version_2/js/script.js
+++ proj_version_2/js/script.js
@@ -4,10 +4,30 @@ const app = new Vue({
   el: '#root',
   data: {
     discs: [],
+    filteredDiscs: [],
+    genre: '',
+  },
+  computed: {
+    getUniqueGenres() {
+      return [...new Set (this.discs.map(disc => {
+        return disc.genre
+      }))]
+    }
+  },
+  methods: {
+    fetchDiscs(){
+      axios.get(`http://localhost:8888/php-ajax-dischi/api/dischi?genre=${this.genre}`).then(res => {
+        this.discs = res.data;
+        this.filteredDiscs = this.discs
+      });
+    },
+    fetchDiscsGenre() {
+      axios.get(`http://localhost:8888/php-ajax-dischi/api/dischi?genre=${this.genre}`).then(res => {
+        this.filteredDiscs = res.data;
+      });
+    },
   },
   mounted() {
-    axios.get('http://localhost:8888/php-ajax-dischi/api/dischi').then(res => {
-      this.discs = res.data;
-    });
-  }
+    this.fetchDiscs();
+  },
 });
